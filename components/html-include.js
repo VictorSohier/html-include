e47d88c70833ab63c11e5f6a86a728253425dfc5
Victor Sohier <1sohiervic@gmail.com>
2023-11-20 15:30:16 -0500

    init

---
+++ components/html-include.js
@@ -0,0 +1,30 @@
+class HxInclude extends HTMLElement
+{
+	constructor()
+	{
+		super();
+		const src = this.getAttribute("src");
+		const xhr = new XMLHttpRequest();
+		const component = this;
+		xhr.onreadystatechange = function()
+		{
+			switch (xhr.readyState)
+			{
+				case(4):
+				{
+					const element = document
+						.createElement("template");
+					element.innerHTML = xhr.responseText;
+					const scripts = element.content.querySelectorAll("script");
+					component.outerHTML = xhr.responseText;
+					for (let i = 0; i < scripts.length; i++)
+						eval(scripts[i].innerText);
+				}
+			}
+		}
+		xhr.open("GET", src, true);
+		xhr.send();
+	}
+}
+
+window.customElements.define("html-include", HxInclude);
